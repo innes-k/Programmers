18fc5e7c20d7c5cd7a4bf74e4bacc9489c7f3519
innes-k <148458439+innes-k@users.noreply.github.com>
2024-01-20 16:24:30 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,9 +1,4 @@
 function solution(s) {
-    // 문자열 s -> split으로 문자열 배열로 만들기
-    // 홀수인 경우 : (첫인덱스 + 마지막인덱스)/2 의 인덱스를 가진 문자 꺼내기
-    // 짝수인 경우 : 
-    // (length/2 -1), length/2 의 인덱스값 꺼내기
-    
     let arr = s.split('')
     if(arr.length % 2 !== 0) {
         let idx = (arr.length-1)/2;
@@ -14,5 +9,4 @@ function solution(s) {
         let idx3 = arr.length/2
         return (s[idx2]+s[idx3]);
     }
-    
 }
